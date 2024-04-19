2897b8222a8b0134d9c1952e4f70e5cbb0730d55
Anushree Jha <anushreejha0604@gmail.com>
2024-04-20 01:30:21 +0530

    works 1.3

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
   buttons.forEach(button => {
     button.addEventListener('click', () => {
       button.classList.toggle('on');
+      if (button.classList.contains('on')) {
+        button.style.transform = 'scale(0.95)';
+      } else {
+        button.style.transform = 'scale(1)';
+      }
     });
   });
 });
